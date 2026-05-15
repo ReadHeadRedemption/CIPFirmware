d7abbecdded6f23393458b52d587e36e0ddf8723
Ethan Baker <45822001+ReadHeadRedemption@users.noreply.github.com>
2026-05-15 05:38:07 -0400

    idfk
    it works? but the motor is grinding might try and roll back and edit from an earlier version

--- CIP_Firmware/main/main.c
+++ CIP_Firmware/main/main.c
@@ -10,7 +10,7 @@ List of Tasks
 - Extruder
   - Needs to read the ink head tool change from G-code
 Heat Bed
-- turn on/off ssr 
+- turn on/off ssr
 - detect temperature
 Screen Display
 Touch Interface
@@ -24,26 +24,25 @@ char *spiffs_file = "/spiffs/sample.gcode";
 
 ///////////////////////////////////////////////////////////////////////////////
 //
-//                          TASKS 
+//                          TASKS
 //
 ///////////////////////////////////////////////////////////////////////////////
 void HeaterControl(void *pvParameters)
 {
-    while(1)
+    while (1)
     {
-        //Read temperature from ADC
-        //Compare with desired temperature
-        //Control SSR accordingly
+        // Read temperature from ADC
+        // Compare with desired temperature
+        // Control SSR accordingly
         vTaskDelay(100 / portTICK_PERIOD_MS); // Delay for 100ms
     }
 }
 
-
 void parserTask(void *pvParameters)
 {
-    while(1)
+    while (1)
     {
-        if(xSemaphoreTake(StartButtonSemaphore, portMAX_DELAY) == pdTRUE)
+        if (xSemaphoreTake(StartButtonSemaphore, portMAX_DELAY) == pdTRUE)
         {
             ESP_LOGI(TAG, "Starting G-code parser task...");
             parse(spiffs_file);
@@ -59,52 +58,73 @@ void parserTask(void *pvParameters)
 ///////////////////////////////////////////////////////////////////////////////
 void moveMotor(void *pvParameters)
 {
-    ESP_LOGI(TAG, "Starting motor test task...");
-    MoveCmd_t moveTo; 
-    moveTo.target_x = 0.0f;
+    if (xSemaphoreTake(TestButtonSemaphore, portMAX_DELAY) == pdTRUE)
+    {
+        ESP_LOGI(TAG, "Starting motor test task...");
+        homeMotors();
+        MoveCmd_t moveTo;
+        moveTo.target_x = 0.0f;
+        moveTo.target_y = 0.0f;
+        moveTo.target_z = 0.0f;
+        moveTo.feed_rate_hz = 100000.0f;
+        while (1)
+        {
+            if (xSemaphoreTake(TestButtonSemaphore, portMAX_DELAY) == pdTRUE)
+            {
+                ESP_LOGI(TAG, "Test button pressed, incrementing motor");
+                vTaskDelay(pdMS_TO_TICKS(1000));
+                moveTo.target_x += 20.0f;
+                moveTo.target_y += 20.0f;
+                moveTo.target_z += 20.0f;
+
+                ESP_LOGI(TAG, "moving motor to X: %.3f Y: %.3f Z: %.3f", moveTo.target_x, moveTo.target_y, moveTo.target_z);
+                vTaskDelay(pdMS_TO_TICKS(1000));
+                coordinated_move(&moveTo);
+                ESP_LOGI(TAG, "Finished Moving");
+                // Reset test button semaphore for next test
+            }
+        }
+    }
+}
+
+void testYaxis(void *pvParameters)
+{
+    homeMotors();
+    MoveCmd_t moveTo;
+    moveTo.feed_rate_hz = 1000.0f;
+    moveTo.target_z = 5.0f;
+    moveTo.target_x = 100.0f;
     moveTo.target_y = 0.0f;
-    moveTo.target_z= 0.0f;
-    moveTo.feed_rate_hz = 10000.0;
+    coordinated_move(&moveTo);
+    moveTo.feed_rate_hz = 1000.0f;
     while(1)
     {
-        if(xSemaphoreTake(TestButtonSemaphore, portMAX_DELAY) == pdTRUE)
-        {
-            ESP_LOGI(TAG, "Test button pressed, incrementing motor");
-            vTaskDelay(pdMS_TO_TICKS(1000));
-            moveTo.target_x += 20.0f;
-            moveTo.target_y += 20.0f;
-            moveTo.target_z += 20.0f;
-
-            ESP_LOGI(TAG, "moving motor to X: %.3f Y: %.3f Z: %.3f", moveTo.target_x, moveTo.target_y, moveTo.target_z);
-            vTaskDelay(pdMS_TO_TICKS(1000));
-            coordinated_move(&moveTo);
-            ESP_LOGI(TAG, "Finished Moving");
-            // Reset test button semaphore for next test
-        }
+        moveTo.target_y -= .1;
+        coordinated_move(&moveTo);
     }
 }
 
 void buttonTest(void *pvParameters)
 {
-    while(1)
+    while (1)
     {
-        if(xSemaphoreTake(TestButtonSemaphore, pdMS_TO_TICKS(10)) == pdTRUE)
+        if (xSemaphoreTake(TestButtonSemaphore, pdMS_TO_TICKS(10)) == pdTRUE)
         {
             ESP_LOGI(TAG, "Test Button Pressed");
         }
-        if(xSemaphoreTake(StartButtonSemaphore, pdMS_TO_TICKS(10)) == pdTRUE)
+        if (xSemaphoreTake(StartButtonSemaphore, pdMS_TO_TICKS(10)) == pdTRUE)
         {
             ESP_LOGI(TAG, "Start Button Pressed");
         }
-        if(xSemaphoreTake(xSwitchSemaphore, pdMS_TO_TICKS(10)) == pdTRUE)
+        if ((xSemaphoreTake(xSwitchSemaphore, pdMS_TO_TICKS(10)) == pdTRUE))
         {
             ESP_LOGI(TAG, "X Switch Pressed");
         }
-        if(xSemaphoreTake(ySwitchSemaphore, pdMS_TO_TICKS(10)) == pdTRUE)
+        if ((xSemaphoreTake(ySwitchSemaphore, pdMS_TO_TICKS(10)) == pdTRUE))
         {
             ESP_LOGI(TAG, "Y Switch Pressed");
         }
-        if(xSemaphoreTake(zSwitchSemaphore, pdMS_TO_TICKS(10)) == pdTRUE)
+        if ((xSemaphoreTake(zSwitchSemaphore, pdMS_TO_TICKS(10)) == pdTRUE))
         {
             ESP_LOGI(TAG, "Z Switch Pressed");
         }
@@ -115,41 +135,47 @@ void buttonTest(void *pvParameters)
 //                          INTERRUPT HANDLES
 //
 ///////////////////////////////////////////////////////////////////////////////
-void IRAM_ATTR xISRHandler(void *arg) {
-    ESP_LOGV(TAG,"X Limit Switch Triggered \n");
+void IRAM_ATTR xISRHandler(void *arg)
+{
+    ESP_LOGV(TAG, "X Limit Switch Triggered \n");
     BaseType_t xHigherPrioTaskWoken = pdFALSE;
     xSemaphoreGiveFromISR(xSwitchSemaphore, &xHigherPrioTaskWoken);
     portYIELD_FROM_ISR(xHigherPrioTaskWoken);
 }
 
-void IRAM_ATTR yISRHandler(void *arg) {
-    ESP_LOGV(TAG,"Y Limit Switch Triggered \n");
+void IRAM_ATTR yISRHandler(void *arg)
+{
+    ESP_LOGV(TAG, "Y Limit Switch Triggered \n");
     BaseType_t xHigherPrioTaskWoken = pdFALSE;
     xSemaphoreGiveFromISR(ySwitchSemaphore, &xHigherPrioTaskWoken);
     portYIELD_FROM_ISR(xHigherPrioTaskWoken);
 }
 
-void IRAM_ATTR zISRHandler(void *arg) {
-    ESP_LOGV(TAG,"Z Limit Switch Triggered \n");
+void IRAM_ATTR zISRHandler(void *arg)
+{
+    ESP_LOGV(TAG, "Z Limit Switch Triggered \n");
     BaseType_t xHigherPrioTaskWoken = pdFALSE;
     xSemaphoreGiveFromISR(zSwitchSemaphore, &xHigherPrioTaskWoken);
     portYIELD_FROM_ISR(xHigherPrioTaskWoken);
 }
 
-void IRAM_ATTR eISRHandler(void *arg) {
-    ESP_LOGV(TAG,"E Limit Switch Triggered \n");
+void IRAM_ATTR eISRHandler(void *arg)
+{
+    ESP_LOGV(TAG, "E Limit Switch Triggered \n");
     BaseType_t xHigherPrioTaskWoken = pdFALSE;
     xSemaphoreGiveFromISR(eSwitchSemaphore, &xHigherPrioTaskWoken);
     portYIELD_FROM_ISR(xHigherPrioTaskWoken);
 }
-void IRAM_ATTR StartButtonISRHandler(void *arg) {
-    ESP_LOGV(TAG,"Start Button Triggered \n");
+void IRAM_ATTR StartButtonISRHandler(void *arg)
+{
+    ESP_LOGV(TAG, "Start Button Triggered \n");
     BaseType_t xHigherPrioTaskWoken = pdFALSE;
     xSemaphoreGiveFromISR(StartButtonSemaphore, &xHigherPrioTaskWoken);
     portYIELD_FROM_ISR(xHigherPrioTaskWoken);
 }
-void IRAM_ATTR TestButtonISRHandler(void *arg) {
-    ESP_LOGV(TAG,"Test Button Triggered \n");
+void IRAM_ATTR TestButtonISRHandler(void *arg)
+{
+    ESP_LOGV(TAG, "Test Button Triggered \n");
     BaseType_t xHigherPrioTaskWoken = pdFALSE;
     xSemaphoreGiveFromISR(TestButtonSemaphore, &xHigherPrioTaskWoken);
     portYIELD_FROM_ISR(xHigherPrioTaskWoken);
@@ -174,13 +200,14 @@ void app_main(void)
     };
 
     esp_err_t ret = esp_vfs_spiffs_register(&conf);
-    if (ret != ESP_OK) {
+    if (ret != ESP_OK)
+    {
         ESP_LOGE(TAG, "Failed to mount SPIFFS");
         return;
     }
     ESP_LOGI(TAG, "SPIFFS mounted successfully");
 
-    // Creating limit switch semaphores 
+    // Creating limit switch semaphores
     xSwitchSemaphore = xSemaphoreCreateBinary();
     ySwitchSemaphore = xSemaphoreCreateBinary();
     zSwitchSemaphore = xSemaphoreCreateBinary();
@@ -194,15 +221,15 @@ void app_main(void)
     //
     //////////////////////////////////////////////////////////////////////////////
 
-    //Setting Pin Directions
+    // Setting Pin Directions
     ESP_LOGI(TAG, "Configuring GPIO pins...");
     ESP_LOGI(TAG, "Configuring Output pins...");
     // Configure stepper direction pins as outputs
     gpio_config_t outputPins = {
-        .pin_bit_mask = (1ULL << xDir) | (1ULL << yDir) | 
+        .pin_bit_mask = (1ULL << xDir) | (1ULL << yDir) |
                         (1ULL << zDir) | (1ULL << eDir) |
-                        (1ULL << xStep)| (1ULL << yStep)| 
-                        (1ULL << zStep)| (1ULL << eStep),
+                        (1ULL << xStep) | (1ULL << yStep) |
+                        (1ULL << zStep) | (1ULL << eStep),
         .mode = GPIO_MODE_OUTPUT,
         .pull_down_en = GPIO_PULLDOWN_DISABLE,
         .pull_up_en = GPIO_PULLUP_DISABLE,
@@ -211,8 +238,8 @@ void app_main(void)
     gpio_config(&outputPins);
     ESP_LOGI(TAG, "Configuring Input pins...");
     gpio_config_t inputPins = {
-        .pin_bit_mask = (1ULL << xSwitch) | (1ULL << ySwitch) | 
-                        (1ULL << zSwitch) | (1ULL << eSwitch) | 
+        .pin_bit_mask = (1ULL << xSwitch) | (1ULL << ySwitch) |
+                        (1ULL << zSwitch) | (1ULL << eSwitch) |
                         (1ULL << StartButton) | (1ULL << TestButton),
         .mode = GPIO_MODE_INPUT,
         .pull_down_en = GPIO_PULLDOWN_DISABLE,
@@ -223,11 +250,11 @@ void app_main(void)
     ESP_LOGI(TAG, "GPIO pins configured successfully");
 
     gpio_install_isr_service(0);
-    gpio_set_intr_type(xSwitch, GPIO_INTR_NEGEDGE);
+    gpio_set_intr_type(xSwitch, GPIO_INTR_POSEDGE);
     gpio_isr_handler_add(xSwitch, xISRHandler, NULL);
-    gpio_set_intr_type(ySwitch, GPIO_INTR_NEGEDGE);
+    gpio_set_intr_type(ySwitch, GPIO_INTR_POSEDGE);
     gpio_isr_handler_add(ySwitch, yISRHandler, NULL);
-    gpio_set_intr_type(zSwitch, GPIO_INTR_NEGEDGE);
+    gpio_set_intr_type(zSwitch, GPIO_INTR_POSEDGE);
     gpio_isr_handler_add(zSwitch, zISRHandler, NULL);
     gpio_set_intr_type(eSwitch, GPIO_INTR_NEGEDGE);
     gpio_isr_handler_add(eSwitch, eISRHandler, NULL);
@@ -238,7 +265,7 @@ void app_main(void)
 
     ESP_LOGI(TAG, "GPIO interrupts configured successfully");
 
-    //ESP_ERROR_CHECK(gpio_config(&outputPins));
+    // ESP_ERROR_CHECK(gpio_config(&outputPins));
 
     //////////////////////////////////////////////////////////////////////////
     //
@@ -246,17 +273,20 @@ void app_main(void)
     //
     /////////////////////////////////////////////////////////////////////////
     // Initialize stepper motors
-    if (stepper_motor_init() != ESP_OK) {
+    if (stepper_motor_init() != ESP_OK)
+    {
         ESP_LOGE(TAG, "Failed to initialize stepper motors!");
         return;
     }
+    // Test Tasks
     //xTaskCreate(moveMotor, "MoveMotor", 4096, NULL, 2, NULL);
-    //xTaskCreate(buttonTest, "Testing button inputs", 2048, NULL, 2, NULL);
-    // Create heater control task
-    //xTaskCreate(HeaterControl, "HeaterControl", 2048, NULL, 1, NULL);
+    //xTaskCreate(testYaxis, "stop yaxis grinding", 4096, NULL, 2, NULL);
+    // xTaskCreate(buttonTest, "Testing button inputs", 2048, NULL, 2, NULL);
 
+    //Create heater control task
+    // xTaskCreate(HeaterControl, "HeaterControl", 2048, NULL, 1, NULL);
     // Create G-code parser task
     xTaskCreate(parserTask, "GCodeParser", 4096, NULL, 3, NULL);
 
-    //ESP_LOGI(TAG, "All tasks created successfully");
+    // ESP_LOGI(TAG, "All tasks created successfully");
 }
--- CIP_Firmware/spiffs_image/sample.gcode
+++ CIP_Firmware/spiffs_image/sample.gcode
@@ -1,12 +1,26 @@
 G21             ; units = mm
 G90             ; absolute coordinates
 G28             ; home all axes
-G1 Z5 F6000     ; safe travel height
-G0 X20 Y50
-G1 X20 Y50 ;F1200
-G1 X20 Y100 F1200
-G1 X20 Y75
-G1 X50 Y75 F1200
-G1 X50 Y100
-G1 X50 Y50
-G0 X0 Y0
+G1 X10
+G4 P5
+G1 Y10
+G4 P5
+G1 X20
+G4 P5
+G1 Y20
+G4 P5
+G1 X30
+G4 P5
+G1 Y30
+G4 P5
+G1 X40
+G4 P5
+G1 Y40
+G4 P5
+G1 X50
+G4 P5
+G1 Y50
+G4 P5
+G1 X 100 Y100
+G4 P5
+G1 X0 Y0
